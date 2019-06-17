c6886419ae1825ff42cd66cd4ce5d06c6b091a31
Andreas Hartono <ahartono@ahartonos-MacBook-Pro.local>
2019-06-17 23:17:06 +0700

    Add existing file

---
+++ app.groovy
@@ -0,0 +1,24 @@
+import org.springframework.core.env.*
+import java.util.Date
+
+@Controller
+class Application {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String index(Model model) {
+		return "<body bgcolor='green'><h1>Hello Groovy World!</h1></body>"
+	}
+
+	@RequestMapping("/ping")
+	@ResponseBody
+	public String ping() {
+		return "Pong: " + new Date()
+	}
+
+	@RequestMapping("/killme")
+	public void killme() {
+		System.exit(-1)
+	}
+
+}
