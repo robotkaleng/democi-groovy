33b1b296f9016dd77ec3c1368103e61660937f37
Andreas Hartono <ahartono@ahartonos-MacBook-Pro.local>
2019-06-18 14:00:39 +0700

    changes

--- app.groovy
+++ app.groovy
@@ -7,7 +7,7 @@ class Application {
 	@RequestMapping("/")
 	@ResponseBody
 	public String index(Model model) {
-		return "<body bgcolor='green'><h1>Hello Groovy World!</h1></body>"
+		return "<body bgcolor='blue'><h1>Hello Groovy World!</h1></body>"
 	}
 
 	@RequestMapping("/ping")
