93ff5dc5eef5440053fb65db7dfc66f3916f7671
Andreas Hartono <ahartono@ahartonos-MacBook-Pro.local>
2019-08-27 08:14:56 +0800

    changes

--- 00-delete_pipeline.sh
+++ 00-delete_pipeline.sh
@@ -2,6 +2,7 @@
 export CC_TARGET=hello  # change it and export your target name
 fly -t $CC_TARGET dp -p groovy-pingpong
 cf login -a https://api.run.pivotal.io -u ahartono@pivotal.io -p Pivnetpasspass123! -o APJ -s development
-cf delete groovy-pingpong-qa
-cf delete groovy-pingpong-prd
-cf delete test-app-groovy
+cf delete -f groovy-pingpong-qa
+cf delete -f groovy-pingpong-prd
+cf delete -f test-app-groovy
+cf delete -f test-app-bca
